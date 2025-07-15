2d17c6af49e7430b1d652b8db6c0b96d10301db3
Fraimer De La Cruz <77998859+fraimerr@users.noreply.github.com>
2025-07-15 10:47:14 -0400

    add proton redirect

---
+++ src/app/(redirects)/proton/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+const Proton = () => {
+	const protonInvite =
+		"https://discord.com/oauth2/authorize?client_id=1358977421621071894";
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			window.location.href = protonInvite;
+		}, 1500);
+
+		return () => clearTimeout(timer);
+	}, [protonInvite]);
+
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<h1 className="text-3xl font-semibold">Redirecting to Proton</h1>
+		</div>
+	);
+};
+
+export default Proton;
