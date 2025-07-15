292f5e5ddd701371212d054219aed1f6655e287e
Fraimer De La Cruz <77998859+fraimerr@users.noreply.github.com>
2025-07-14 23:46:27 -0400

    add my flag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div className="flex items-center justify-center min-h-screen">
-			<h1 className="text-9xl font-bold">Hi, I&apos;m Fraimer.</h1>
+			<h1 className="text-9xl font-bold">Hi, I&apos;m Fraimer. 🇩🇴</h1>
 		</div>
 	);
 }
